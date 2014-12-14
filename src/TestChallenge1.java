6119cec6d724b197b81ef9b5057c4e1e95b3f8b3
Kent Collins <kent.r.collins@gmail.com>
2014-12-14 14:35:36 +0000

    updated

--- src/TestChallenge1.java
+++ src/TestChallenge1.java
@@ -8,7 +8,7 @@ public class TestChallenge1 {
 	public static void main(String[] args) {
 		// TODO Load a default map at size 20 by 20
 		// TODO Create a Squarebot and have it place beepers at the corners of a rectangle
-		//   	with bottom right corner at (9, 3) that is 5 units on each side (see picture)
+		//   	with bottom right corner at (9, 3) and sized for 5 beepers on each side (see picture)
 		// TODO Create a Sentry and have it patrol clockwise around the beepers, indefinitely
 	}
 
--- src/TestChallenge3.java
+++ src/TestChallenge3.java
@@ -6,7 +6,7 @@ public class TestChallenge3 {
 	// TODO Complete the BeepFarmer class, provided, by implementing the reap() method
 
 	public static void main(String[] args) {
-		// TODO Load the "farm" map at size 25 by 25
+		// TODO Load the "farm" map at size 10 by 10
 		// TODO Create an instance of a BeepFarmer with no beepers, anywhere on the farm
 		// TODO Have the farmer gather all the beepers from the garden plot in the center of the map
 		// TODO Have the farmer drop at location (1, 1) all the beepers picked and then move aside.
