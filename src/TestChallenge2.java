e3db2e9fec126a1b6b45ee380234fbd62bbfc565
Kent Collins <kent.r.collins@gmail.com>
2014-12-14 20:01:53 +0000

    Update TestChallenge2.java

--- src/TestChallenge2.java
+++ src/TestChallenge2.java
@@ -8,7 +8,7 @@ public class TestChallenge2 {
 	public static void main(String[] args) {
 		// TODO Load the "pasture" map at size 15 by 15
 		// TODO Create an instance of a Horse inside the pasture
-		// TODO Have the horse wander for 36 steps with a timer of 5
+		// TODO Have the horse wander for 36 steps with a timer of 7
 		// TODO Have the horse explode()
 	}
 
