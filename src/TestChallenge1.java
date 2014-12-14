d59630fd2e75acb50ef9259ec3d058dfcc176e85
Kent Collins <kent.r.collins@gmail.com>
2014-12-14 14:29:18 +0000

    updated

--- src/TestChallenge1.java
+++ src/TestChallenge1.java
@@ -3,13 +3,13 @@
  * 
  */
 public class TestChallenge1 {
+	// TODO Complete the provided Sentry class by implementing its patrol() method
 
 	public static void main(String[] args) {
 		// TODO Load a default map at size 20 by 20
 		// TODO Create a Squarebot and have it place beepers at the corners of a rectangle
 		//   	with bottom right corner at (9, 3) that is 5 units on each side (see picture)
-		// TODO Complete the provided Sentry class by implementing the patrol() method
-		// TODO Create a Sentry and have it patrol the square of beepers, indefinitely
+		// TODO Create a Sentry and have it patrol clockwise around the beepers, indefinitely
 	}
 
 }
